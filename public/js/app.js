02be2c0426b33b2d885756e40b4f18aa101b9d53
Caleb Withers <blueboltgamingyt@gmail.com>
2026-09-01 15:27:03 -0500

    Delete public directory

--- public/js/app.js
+++
@@ -1,8 +0,0 @@
-async function api(url, body) {
-  const r = await fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(body),
-  });
-  return r.json();
-}
